c6d0606c8369ca2e07d9111eb5c7daf742833776
Hubert Rutkowski <hubr2000@gmail.com>
2022-12-31 02:41:37 +0100

    move react query provider up - to mianpage

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -5,9 +5,13 @@ import Learn from "../scenes/Learn";
 import Edit from "../scenes/Edit";
 import ModalInput from "../elements/ModalInput";
 import { useSearchParams } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 type Props = {};
 type ModeType = typeof Mode[keyof typeof Mode];
+
+const queryClient = new QueryClient();
 
 const MainPage: React.FC<Props> = () => {
 	const [mode, setMode] = useState<ModeType>(Mode.EDIT);
@@ -15,24 +19,27 @@ const MainPage: React.FC<Props> = () => {
 	const [info, setInfo] = useSearchParams();
 
 	return (
-		<PageWrapper
-			mode={mode}
-			setMode={setMode}
-			setShowModal={setShowModal}
-			showModal={showModal}>
-			{mode === Mode.LEARN && info.get("set") && (
-				<Learn setName={info.get("set")} />
-			)}
-			{mode === Mode.EDIT && info.get("set") && (
-				<Edit setName={info.get("set")} />
-			)}
-			{showModal && (
-				<ModalInput
-					showModal={showModal}
-					setShowModal={setShowModal}
-				/>
-			)}
-		</PageWrapper>
+		<QueryClientProvider client={queryClient}>
+			<PageWrapper
+				mode={mode}
+				setMode={setMode}
+				setShowModal={setShowModal}
+				showModal={showModal}>
+				{mode === Mode.LEARN && info.get("set") && (
+					<Learn setName={info.get("set")} />
+				)}
+				{mode === Mode.EDIT && info.get("set") && (
+					<Edit setName={info.get("set")} />
+				)}
+				{showModal && (
+					<ModalInput
+						showModal={showModal}
+						setShowModal={setShowModal}
+					/>
+				)}
+			</PageWrapper>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 };
 
